fa7c4d85e9a361f53e4f746f44666c2a6ac69b6e
reuben-idan <nobeadonis2@gmail.com>
2025-02-18 11:05:52 +0000

    update contact

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-let contact = {};
+let contact = {
+  tel: "207-662-5412",
+  email: "RonaldSMurphy@freepost.org",
+};
+
+
+contact.firstName = "Ronald";
+contact.lastName = "Murphy";
 contact.tel = "207-662-5412";
+
+contact.email ={ work: "RonaldSMurphy@freepost.org", personal: "reuben.idan@yahoo.com" };
+
+
 console.log(contact);
-console.log(contact.tel);
+
+console.log(contact.email.personal);
